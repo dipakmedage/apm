0d1112aba215cc77a061b83c86f01179b16e0593
Dipak Medage <18dipak@gmail.com>
2020-07-07 15:05:30 +0530

    untracked files

--- apm/src/main/java/com/nclinic/apm/reositories/PatientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nclinic.apm.reositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.nclinic.apm.models.Patient;
-
-public interface PatientRepository extends CrudRepository<Patient, Long> {
-
-}
